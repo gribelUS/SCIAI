313a29d8ac586c61422f3824096fb8e1e1a0f12e
Tiago Breunig <85698970+gribelUS@users.noreply.github.com>
2025-09-18 23:02:03 -0400

    User check
    Initial changes for checking input password from logged user, not implemented in the database

--- front-end/gui/navbar.py
+++ front-end/gui/navbar.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QSizePolicy, QPushButton, QVBoxLayout, QInputDialog, QLineEdit, QMessageBox
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
+from login import check_logged_password
 
 class NavBar(QWidget):
     def __init__(self, user):
@@ -56,8 +57,8 @@ class NavBar(QWidget):
             self.mode_switch_btn = QPushButton("PLC Mode")
             self.mode_switch_btn.setCursor(Qt.PointingHandCursor)
             self.mode_switch_btn.setCheckable(True)
-            self.mode_switch_btn.clicked.connect(self.toggle_mode)
             self.mode_switch_btn.setStyleSheet("background-color: white; color: #002855; border-radius: 5px;")
+            self.mode_switch_btn.clicked.connect(self.check_logged_password)
             layout.addWidget(self.mode_switch_btn)
 
         else:
@@ -122,8 +123,16 @@ class NavBar(QWidget):
             self.activity_btn.setChecked(False)
             self.manage_users_btn.setChecked(True)
 
+    def check_logged_password(self):
+        password = QMessageBox("Enter Password", "Please enter your password to switch modes:", QMessageBox.Ok / QMessageBox.Cancel)
+        if check_logged_password(password):
+            self.toggle_mode()
+        else:
+            QMessageBox.warning(self, "Error", "Incorrect password. Mode switch aborted.")
+            self.mode_switch_btn.setChecked(not self.mode_switch_btn.isChecked())
+
     def toggle_mode(self):
-        
+
         if self.mode_switch_btn.isChecked():
             self.mode_switch_btn.setText("Web Mode")
             self.mode_switch_btn.setStyleSheet("background-color: #002855; color: white; border-radius: 5px;")
